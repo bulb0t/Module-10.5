07b3d1b24857adff63ee71056c1b2e090af64608
bulb0t <yu.uglenko@yandex.ru>
2024-12-08 15:28:30 +0300

    Add files via upload

---
+++ Module 10.5.py
@@ -0,0 +1,32 @@
+import os
+from time import time
+import multiprocessing
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as file:
+        for _ in file:
+            if file.readline() == '':
+                break
+            all_data.append(file.readline())
+
+names_files = [name for name in os.listdir() if os.path.isfile(name) and name.endswith('.txt')]
+
+# start_time = time()
+#
+# for name in names_files:
+#     read_info(name)
+#
+# end_time = time()
+#
+# time_1 = end_time - start_time
+# print((time_1))
+
+if __name__ == '__main__':
+    start_time = time()
+    with multiprocessing.Pool(len(names_files)) as p:
+        p.map(read_info, names_files)
+    end_time = time()
+
+    time_2 = end_time - start_time
+    print((time_2))
